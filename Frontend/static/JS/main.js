61b016304c6e93d0d863c56d07d08d21a61715a3
Santiago <marmoljuliosantiago@gmail.com>
2024-06-07 10:52:18 -0500

    Arreglo de errores en la transición  del login

--- Frontend/static/JS/main.js
+++ Frontend/static/JS/main.js
@@ -1,13 +1,9 @@
-let subDropdown = document.getElementById('subDropdown')
-
-function toggleMenu() {
-    subDropdown.classList.toggle('open-menu')
-}
-
-
 document.addEventListener('DOMContentLoaded', function () {
     const sidebarLinks = document.querySelectorAll('.sidebar-link');
-
+    const submenus = document.querySelectorAll('.submenu');
+    const paginationActivos = document.getElementById('pagination-activos');
+    const paginationInactivos = document.getElementById('pagination-inactivos');
+    
     sidebarLinks.forEach(link => {
         link.addEventListener('click', function (event) {
             const submenu = this.querySelector('.submenu');
@@ -16,15 +12,13 @@ document.addEventListener('DOMContentLoaded', function () {
             if (!isOpen) {
                 submenu.style.display = 'block';
             }
-            event.stopPropagation(); // Evitar que el evento se propague al elemento padre (sidebar-link)
+            event.stopPropagation();
         });
     });
 
-    // Evitar que se cierre el submenú al hacer clic dentro de él
-    const submenus = document.querySelectorAll('.submenu');
     submenus.forEach(submenu => {
         submenu.addEventListener('click', function (event) {
-            event.stopPropagation(); // Evitar que el evento se propague al elemento padre (sidebar-link)
+            event.stopPropagation();
         });
     });
 
@@ -34,7 +28,20 @@ document.addEventListener('DOMContentLoaded', function () {
             closeAllSubmenus();
         }
     });
+
+    paginationActivos.addEventListener('click', function (event) {
+        event.stopPropagation();
+    });
+
+    paginationInactivos.addEventListener('click', function (event) {
+        event.stopPropagation();
+    });
 });
+
+function toggleMenu() {
+    let subDropdown = document.getElementById('subDropdown');
+    subDropdown.classList.toggle('open-menu');
+}
 
 function closeAllSubmenus() {
     const submenus = document.querySelectorAll('.submenu');
@@ -43,42 +50,10 @@ function closeAllSubmenus() {
     });
 }
 
-const paginationActivos = document.getElementById('pagination-activos');
-const paginationInactivos = document.getElementById('pagination-inactivos');
-
-paginationActivos.addEventListener('click', function (event) {
-    event.stopPropagation();
-});
-
-paginationInactivos.addEventListener('click', function (event) {
-    event.stopPropagation();
-});
-
-
-
-const container = document.getElementById('container');
-const registerBtn = document.getElementById('register');
-const loginBtn = document.getElementById('login');
-
-registerBtn.addEventListener('click', () => {
-    container.classList.add("active");
-});
-loginBtn.addEventListener('click', () => {
-    container.classList.remove("active");
-});
-
-
-
-
-
-
-
-// exportar tabla a formato exel
-
 function ExportToExcel(type, fn, dl) {
     var elt = document.getElementById('myTable');
     var wb = XLSX.utils.table_to_book(elt, { sheet: "sheet1" });
     return dl ?
-      XLSX.write(wb, { bookType: type, bookSST: true, type: 'base64' }):
-      XLSX.writeFile(wb, fn || ('Reporte-SmarkLink.' + (type || 'xlsx')));
- }
+        XLSX.write(wb, { bookType: type, bookSST: true, type: 'base64' }) :
+        XLSX.writeFile(wb, fn || ('Reporte-SmarkLink.' + (type || 'xlsx')));
+}
