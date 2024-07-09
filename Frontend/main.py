eda8e5dd36ace5f8dcb316aa5976d297609ec58c
santiagomarmol <marmoljuliosantiago@gmail.com>
2024-07-09 13:58:53 -0500

    Estandarización

--- Frontend/main.py
+++ Frontend/main.py
@@ -6,7 +6,7 @@ from config.db_queries import authenticate_user, check_email_exists, register_ne
 app = Flask(__name__)
 app.secret_key = os.getenv('KEY')
 
-@app.route("/", methods=['GET', 'POST'])
+@app.route("/", methods=['POST'])
 def login():
     """Route to display the login page and handle login.
 
@@ -30,7 +30,7 @@ def login():
 
     return render_template("login.html")
 
-@app.route("/register", methods=["GET", "POST"])
+@app.route("/register", methods=["POST"])
 def register():
     """Route to display the registration page and handle user registration.
 
