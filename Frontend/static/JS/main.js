f67dbc28678d4c15a18c9e480f29700001ecb4ef
santiagomarmol <marmoljuliosantiago@gmail.com>
2024-07-09 09:09:34 -0500

    Estandarización de html

--- Frontend/static/JS/main.js
+++ Frontend/static/JS/main.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', function () {
     const sidebarLinks = document.querySelectorAll('.sidebar-link');
     const submenus = document.querySelectorAll('.submenu');
-    
+
     sidebarLinks.forEach(link => {
         link.addEventListener('click', function (event) {
             const submenu = this.querySelector('.submenu');
@@ -28,40 +28,30 @@ document.addEventListener('DOMContentLoaded', function () {
     });
 
     // Evitar que los clics en la paginación afecten al menú
-    const paginationInactive = document.getElementById('pagination-inactive');
-    paginationInactive.addEventListener('click', function (event) {
-        event.stopPropagation();
-    });
+    const paginationElements = [
+        'pagination-inactive',
+        'prev-inactive',
+        'next-inactive',
+        'pagination-active',
+        'prev-active',
+        'next-active'
+    ];
 
-    const prevInactive = document.getElementById('prev-inactive');
-    prevInactive.addEventListener('click', function (event) {
-        event.stopPropagation();
-    });
-
-    const nextInactive = document.getElementById('next-inactive');
-    nextInactive.addEventListener('click', function (event) {
-        event.stopPropagation();
-    });
-
-    const paginationActive = document.getElementById('pagination-active');
-    paginationActive.addEventListener('click', function (event) {
-        event.stopPropagation();
-    });
-
-    const prevActive = document.getElementById('prev-active');
-    prevActive.addEventListener('click', function (event) {
-        event.stopPropagation();
-    });
-
-    const nextActive = document.getElementById('next-active');
-    nextActive.addEventListener('click', function (event) {
-        event.stopPropagation();
+    paginationElements.forEach(id => {
+        const element = document.getElementById(id);
+        if (element) {
+            element.addEventListener('click', function (event) {
+                event.stopPropagation();
+            });
+        }
     });
 });
 
 function toggleMenu() {
     let subDropdown = document.getElementById('subDropdown');
-    subDropdown.classList.toggle('open-menu');
+    if (subDropdown) {
+        subDropdown.classList.toggle('open-menu');
+    }
 }
 
 function closeAllSubmenus() {
@@ -73,8 +63,10 @@ function closeAllSubmenus() {
 
 function ExportToExcel(type, fn, dl) {
     var elt = document.getElementById('myTable');
-    var wb = XLSX.utils.table_to_book(elt, { sheet: "sheet1" });
-    return dl ?
-        XLSX.write(wb, { bookType: type, bookSST: true, type: 'base64' }) :
-        XLSX.writeFile(wb, fn || ('Reporte-SmarkLink.' + (type || 'xlsx')));
+    if (elt) {
+        var wb = XLSX.utils.table_to_book(elt, { sheet: "sheet1" });
+        return dl ?
+            XLSX.write(wb, { bookType: type, bookSST: true, type: 'base64' }) :
+            XLSX.writeFile(wb, fn || ('Reporte-SmarkLink.' + (type || 'xlsx')));
+    }
 }
