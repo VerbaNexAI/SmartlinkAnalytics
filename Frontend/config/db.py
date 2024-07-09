82e5f3ca8d7f5f5ee8c962722c64ec258adf4ac7
santiagomarmol <marmoljuliosantiago@gmail.com>
2024-07-09 11:06:57 -0500

    Estandarización

--- Frontend/config/db.py
+++ Frontend/config/db.py
@@ -2,7 +2,6 @@ import pyodbc
 import os
 from dotenv import load_dotenv
 
-# Load environment variables from .env file
 load_dotenv()
 
 def conn_db():
@@ -21,7 +20,6 @@ def conn_db():
     print(f'Attempting to connect to the database with: SERVER={server}, DATABASE={db_name}, USER={user}')
 
     try:
-        # Attempt to establish connection with provided credentials
         connection = pyodbc.connect(
             'DRIVER={ODBC Driver 17 for SQL Server};'
             f'SERVER={server};'
@@ -32,14 +30,11 @@ def conn_db():
         print('CONNECTION SUCCESSFUL')
         return connection
     except Exception as e:
-        # Capture and handle connection errors
         print('Connection error:', e)
         return None
 
-# Call the function to establish the connection
 connection = conn_db()
 
-# Example of executing a query after establishing the connection
 if connection:
     cursor = connection.cursor()
     cursor.execute('SELECT @@version')
@@ -47,4 +42,4 @@ if connection:
     if row:
         print('Server version:', row[0])
     cursor.close()
-    connection.close()  # It's important to close the connection when no longer needed
+    connection.close()
